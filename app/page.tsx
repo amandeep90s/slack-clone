8422cd9ec3854d545bef54cf6908ea5f6294a0ab
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-06-09 22:52:41 +0530

    feat: add Google provider to authentication and implement sign-out functionality in the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
+
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/web/theme-toggle";
 
 export default function Home() {
+  const { signOut } = useAuthActions();
+
   return (
     <div>
       Slack Clone
       <ThemeToggle />
+      <Button onClick={signOut}>Sign Out</Button>
     </div>
   );
 }
